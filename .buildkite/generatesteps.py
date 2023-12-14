c2a1ed1dedc1666d5dfa0a5a1e92abcb3eaf0d2a
Quentin Pradet <quentin.pradet@elastic.co>
2023-12-14 21:09:08 +0400

    Group run/teardown steps

--- .buildkite/generatesteps.py
+++ .buildkite/generatesteps.py
@@ -6,45 +6,48 @@ import yaml
 def benchmark_to_steps(python, connection_class):
     return [
         {
-            "label": f":elasticsearch: :python: ES Serverless ({python}/{connection_class})",
-            "agents": {"provider": "gcp"},
-            "env": {
-                "PYTHON_VERSION": f"{python}",
-                "PYTHON_CONNECTION_CLASS": f"{connection_class}",
-                # TEMPORARY for 3.11
-                # https://github.com/aio-libs/aiohttp/issues/6600
-                "AIOHTTP_NO_EXTENSIONS": 1,
-                # https://github.com/aio-libs/frozenlist/issues/285
-                "FROZENLIST_NO_EXTENSIONS": 1,
-                # https://github.com/aio-libs/yarl/issues/680
-                "YARL_NO_EXTENSIONS": 1,
-                "EC_REGISTER_BACKEND": "appex-qa-team-cluster",
-                "EC_ENV": "qa",
-                "EC_REGION": "aws-eu-west-1",
-                "EC_PROJECT_PREFIX": f"esv-client-python-test-{python}-{connection_class}",
-            },
-            "command": "./.buildkite/run-tests",
-            "artifact_paths": "junit/*-junit.xml",
-            "retry": {"manual": False},
-            "key": f"run_{python.replace('.', '_')}_{connection_class}",
-        },
-        {
-            "label": f":elasticsearch: :python: Teardown ES Serverless ({python}/{connection_class})",
-            "command": "./.buildkite/run-tests",
-            "command": f"bash .buildkite/ci.sh terminate_environment",
-            "agents": {"provider": "gcp"},
-            "env": {
-                "PYTHON_VERSION": f"{python}",
-                "PYTHON_CONNECTION_CLASS": f"{connection_class}",
-                "EC_REGISTER_BACKEND": "appex-qa-team-cluster",
-                "EC_ENV": "qa",
-                "EC_REGION": "aws-eu-west-1",
-                "EC_PROJECT_PREFIX": f"esv-client-python-test-{python}-{connection_class}",
-            },
-            "command": ".buildkite/teardown-tests",
-            "depends_on": f"run_{python.replace('.', '_')}_{connection_class}",
-            "allow_dependency_failure": True,
-        },
+            "group": f":elasticsearch: :python: ES Serverless ({python}/{connection_class})",
+            "steps": [
+                {
+                    "label": "Run tests",
+                    "agents": {"provider": "gcp"},
+                    "env": {
+                        "PYTHON_VERSION": f"{python}",
+                        "PYTHON_CONNECTION_CLASS": f"{connection_class}",
+                        # TEMPORARY for 3.11
+                        # https://github.com/aio-libs/aiohttp/issues/6600
+                        "AIOHTTP_NO_EXTENSIONS": 1,
+                        # https://github.com/aio-libs/frozenlist/issues/285
+                        "FROZENLIST_NO_EXTENSIONS": 1,
+                        # https://github.com/aio-libs/yarl/issues/680
+                        "YARL_NO_EXTENSIONS": 1,
+                        "EC_REGISTER_BACKEND": "appex-qa-team-cluster",
+                        "EC_ENV": "qa",
+                        "EC_REGION": "aws-eu-west-1",
+                        "EC_PROJECT_PREFIX": f"esv-client-python-test-{python}-{connection_class}",
+                    },
+                    "command": "./.buildkite/run-tests",
+                    "artifact_paths": "junit/*-junit.xml",
+                    "retry": {"manual": False},
+                    "key": f"run_{python.replace('.', '_')}_{connection_class}",
+                },
+                {
+                    "label": "Teardown",
+                    "agents": {"provider": "gcp"},
+                    "env": {
+                        "PYTHON_VERSION": f"{python}",
+                        "PYTHON_CONNECTION_CLASS": f"{connection_class}",
+                        "EC_REGISTER_BACKEND": "appex-qa-team-cluster",
+                        "EC_ENV": "qa",
+                        "EC_REGION": "aws-eu-west-1",
+                        "EC_PROJECT_PREFIX": f"esv-client-python-test-{python}-{connection_class}",
+                    },
+                    "command": ".buildkite/teardown-tests",
+                    "depends_on": f"run_{python.replace('.', '_')}_{connection_class}",
+                    "allow_dependency_failure": True,
+                },
+            ],
+        }
     ]
 
 
